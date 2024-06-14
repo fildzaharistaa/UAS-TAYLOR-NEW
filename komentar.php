62803e7594eb1d78614440a4f0f472cee97c3b19
fildzaharistaa <fildzaharistaaa@gmail.com>
2024-06-14 18:16:10 +0700

    udah ak apus yh ti tinggal pull aja

--- komentar.php
+++ komentar.php
@@ -137,7 +137,6 @@
 </main>
 
 <script src="./javascript/main.js"></script>
-<script src="./javascript/validasicomment.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </body>
 </html>
